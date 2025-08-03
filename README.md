e89b5f7fd18a0f9e121c3c7da4169f885dcd4b3b
sgi-demos <sgi.demos@gmail.com>
2025-08-02 23:37:42 -0400

    Add screenshots to README, move txf viewer to own dir

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Collection of fonts from SGI systems and tools for rendering them.
+# sgi-fonts
+*A collection of fonts from SGI systems and tools for rendering them.*
+
+### [TXF fonts](txf) and [TXF viewer](txf-viewer)
+![SGI Helvetica Oblique](sgi-fonts.png)
+
+### [Boot screen fonts](boot)
+![SGI Indy boot screen](sgi-indy-boot.png)
+
+### [Popup menus](popup_menus)
+![SGI popup menu](sgi-pup-menu.png)
--- txf-viewer/draw-texfont.js
+++ txf-viewer/draw-texfont.js
@@ -1,5 +1,5 @@
 // Fork of https://web.archive.org/web/20220527005219/https://discourse.threejs.org/t/texfont-for-three-js/35888
-// 
+//
 import * as THREE from './three.module.js'; // https://unpkg.com/browse/three@0.150.1/build/three.module.js
 import { OrbitControls } from './OrbitControls.js'; // https://unpkg.com/browse/three@0.150.1/examples/jsm/controls/OrbitControls.js
 import { Texfont } from './texfont.js';
@@ -17,7 +17,7 @@ let fontName = 'Helvetica-Oblique';
 // fontName = 'derniere';
 // fontName = 'sorority';
 
-txf.LoadTexfont( 'txf/' + fontName + '.txf', txf );
+txf.LoadTexfont( '../txf/' + fontName + '.txf', txf );
 
 renderer = new THREE.WebGLRenderer({antialias: true, alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -50,7 +50,7 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	
+
 	render();
 
 }
@@ -70,8 +70,8 @@ function render() {
 
 
 		// background rectangle
-		var planeMesh = new THREE.Mesh( 
-			new THREE.PlaneGeometry( txf.tex_width*4, txf.tex_height*4 ), 
+		var planeMesh = new THREE.Mesh(
+			new THREE.PlaneGeometry( txf.tex_width*4, txf.tex_height*4 ),
 			new THREE.MeshBasicMaterial( {color: 0x000000, side: THREE.DoubleSide} ) );
 		planeMesh.position.z -= 1.0;
 		scene.add( planeMesh );
@@ -80,27 +80,27 @@ function render() {
 		scene.add( stringGroup );
 
 		texture_ready = true;
-									
+
 	} else {
 		// automatically consume frames until texture is ready.
 		// After that, update on OrbitControl() 'change' events only.
 		if ( !texture_ready ) requestAnimationFrame( render );
-			
+
 	}
 
 	renderer.render( scene, camera );
-	
+
 }
 
 function setupTexfontMaterial() {
-	
+
 	var width = txf.tex_width;
 	var height = txf.tex_height;
 	var size = width * height;
 	var srcData = new Uint8Array( 4 * size );	// THREE.RGBAFormat
-					
+
 	let i, j, jj, k;
-	
+
 	for(  i = 0; i < height; i++ ) {
 		for ( j = 0; j < width; j++ ) {
 			k = i * width + j;
@@ -118,10 +118,10 @@ function setupTexfontMaterial() {
 	srcMaterial = new THREE.MeshBasicMaterial( {
 		side: THREE.DoubleSide,
 		color: 0xffffff,
-		blending: THREE.NoBlending,	
+		blending: THREE.NoBlending,
 		map: srcTexture
 	} );
-	
+
 	txf.srcMaterial = srcMaterial;
 
 }
